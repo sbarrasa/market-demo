9930249c0366e98bff6c8c704ea0bc4da24b5f3c
t3rry327 <t3rry327@gmail.com>
2023-02-03 18:25:29 -0300

    Repository fix

--- src/main/java/com/blink/springboot/controller/CustomersController.java
+++ src/main/java/com/blink/springboot/controller/CustomersController.java
@@ -6,12 +6,12 @@ import java.util.Optional;
 import java.util.Random;
 
 
+import com.blink.mediamanager.MediaTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.blink.mediamanager.MediaTemplate;
 import com.blink.springboot.entities.Customer;
 import com.blink.springboot.entities.Sex;
 import com.blink.springboot.services.CustomersService;
--- src/main/java/com/blink/springboot/dao/ProductsRepository.java
+++ src/main/java/com/blink/springboot/dao/ProductsRepository.java
@@ -9,33 +9,35 @@ import org.springframework.stereotype.Repository;
 import com.blink.springboot.entities.Product;
 
 @Repository
-public interface  ProductsRepository extends JpaRepository<Product, Long>{
-	default public Product update(Product productUpdate) {
-		Product product = findById(productUpdate.getId()).orElse(productUpdate);
-		
-		if(productUpdate.getName()!=null)
-			product.setName(productUpdate.getName());
-	
-		if(productUpdate.getDescription()!=null)
-			product.setDescription(productUpdate.getDescription());
-		
-		if(productUpdate.getPrice()!=null)
-			product.setPrice(productUpdate.getPrice());
-	
-		if(productUpdate.getStock()!=null)
-			product.setStock(productUpdate.getStock());
-	
-				
-		product.setUpdated(productUpdate.getUpdated());
-		
-		return save(product);
-		
-	}
+public interface ProductsRepository extends JpaRepository<Product, Long> {
+    default public Product update(Product productUpdate) {
+        Product product = productUpdate;
+        if (productUpdate.getId() != null) {
+            product = findById(productUpdate.getId()).orElse(productUpdate);
+        }
 
-	default public List<Product> updateAll(List<Product> products){
-		return products.stream().map(p -> update(p)).collect(Collectors.toList());
-	}
-	
-	
-	
+        if (productUpdate.getName() != null)
+            product.setName(productUpdate.getName());
+
+        if (productUpdate.getDescription() != null)
+            product.setDescription(productUpdate.getDescription());
+
+        if (productUpdate.getPrice() != null)
+            product.setPrice(productUpdate.getPrice());
+
+        if (productUpdate.getStock() != null)
+            product.setStock(productUpdate.getStock());
+
+
+        product.setUpdated(productUpdate.getUpdated());
+
+        return save(product);
+
+    }
+
+    default public List<Product> updateAll(List<Product> products) {
+        return products.stream().map(p -> update(p)).collect(Collectors.toList());
+    }
+
+
 }
